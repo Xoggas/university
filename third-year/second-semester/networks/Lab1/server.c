8acfee9831149a28476da5ea8529836c943b106a
Xoggas <sashakor16@gmail.com>
2025-03-11 14:31:47 +0300

    Updated client and server of lab-1 of Networks

--- third-year/second-semester/networks/Lab1/server.c
+++ third-year/second-semester/networks/Lab1/server.c
@@ -5,27 +5,8 @@
 #define OK 0
 #define DRIVER_IS_NOT_INSTALLED 1
 
-#define SEND_MESSAGES 1
-#define RECEIVE_MESSAGES 2
-
-void print_current_directory()
-{
-    char path[128];
-
-    if (getcwd(path, sizeof(path)) != NULL)
-    {
-        printf("Current dir: %s\n", path);
-    }
-    else
-    {
-        printf("Could'n retrieve current dir!\n");
-    }
-}
-
 int main()
 {
-    print_current_directory();
-
     if (IPX_IsDriverOk() == false)
     {
         printf("IPX driver is not installed\n");
